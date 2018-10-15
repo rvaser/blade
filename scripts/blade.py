b0a463c3af7e3cd2076e12d4dbd46707d61922ae
rvaser <robert.vaser@fer.hr>
2018-10-15 16:19:09 +0200

    Replaced tar compression with gzip

--- scripts/blade.py
+++ scripts/blade.py
@@ -30,16 +30,14 @@ class Runner:
             for directory_name, _, data_names in os.walk(path):
                 directory_path = os.path.abspath(directory_name)
                 if (directory_path not in self.path_dictionary):
-                    self.path_dictionary[directory_path] = (set(), set(), set())
+                    self.path_dictionary[directory_path] = (set(), set())
                 for data_name in data_names:
                     data_path = os.path.join(directory_path, data_name)
-                    if (data_name.endswith(('.fa', '.fasta', '.fq', '.fastq'))):
+                    if (data_name.endswith(('.fa', '.fasta', '.fq', '.fastq', '.fa.gz', '.fasta.gz', '.fq.gz', '.fastq.gz'))):
                         if ('reference' in data_name):
                             self.path_dictionary[directory_path][0].add(data_path)
                         else:
                             self.path_dictionary[directory_path][1].add(data_path)
-                    elif (data_name.endswith(('.tar.gz', '.tgz'))):
-                        self.path_dictionary[directory_path][2].add(data_path)
 
         self.work_directory = os.getcwd() + '/replicant_list'
 
@@ -63,7 +61,7 @@ class Runner:
             if (sum([len(paths) for paths in data_paths]) == 0):
                 continue
 
-            reference_paths, uncompressed_data_paths, compressed_data_paths = data_paths
+            reference_paths, sequencing_data_paths = data_paths
 
             reference_path = next(iter(reference_paths))\
                 if (len(reference_paths) != 0) else ''
@@ -73,7 +71,7 @@ class Runner:
 
             eprint('Blade::Runner::execute processing directory {}'.format(directory_path))
 
-            for data_path in uncompressed_data_paths:
+            for data_path in sequencing_data_paths:
 
                 eprint('--> {} {} {}'.format(\
                     os.path.basename(self.executable),\
@@ -83,36 +81,6 @@ class Runner:
                 p = subprocess.Popen([self.executable, data_path, reference_path],\
                     stdout=runner_report, stderr=runner_report)
                 p.communicate()
-
-            for compressed_data_path in compressed_data_paths:
-                eprint('--[ extracting {}'.format(\
-                    os.path.basename(compressed_data_path)))
-
-                shutil.copy(compressed_data_path, self.work_directory)
-
-                tar_path = os.path.join(self.work_directory,\
-                    os.path.basename(compressed_data_path))
-                tar = tarfile.open(tar_path)
-
-                data_paths = [os.path.join(self.work_directory, data_path)\
-                    for data_path in tar.getnames()]
-
-                tar.extractall(self.work_directory)
-                tar.close()
-                os.remove(tar_path)
-
-                for data_path in data_paths:
-
-                    eprint('    --> {} {} {}'.format(\
-                        os.path.basename(self.executable),\
-                        os.path.basename(data_path),\
-                        os.path.basename(reference_path)))
-
-                    p = subprocess.Popen([self.executable, data_path, reference_path],\
-                        stdout=runner_report, stderr=runner_report)
-                    p.communicate()
-
-                    os.remove(data_path)
 
         runner_report.close()
 
@@ -124,15 +92,16 @@ if __name__ == '__main__':
         the form \'executable reads [reference]\' on a large amount of
         sequencing data.''')
     parser.add_argument('executable', help='''executable which takes reads in
-        FASTA or FASTQ format as positional argument; if present, a reference
-        file in FASTA format (containing the word reference in name) will be
-        passed as second argument''')
-    parser.add_argument('path', nargs='+', help='''path to folder which contains
-        compressed (.tar.gz or .tgz) or uncompressed sequencing data in FASTA or
-        FASTQ format (supported file extensions are .fa, .fasta, .fq and
-        .fastq)''')
+        FASTA/FASTQ format (with gzip support) as positional argument; if present,
+        a file with the reference genome in FASTA format will be passed as the
+        second positional argument (supported file names are those that contain
+        the word reference)''')
+    parser.add_argument('path', nargs='+', help='''path to directory which
+        somewhere in its directory tree contains compressed/uncompressed sequencing
+        data in FASTA/FASTQ format (supported file extensions are .fa, .fasta,
+        .fq, .fastq, .fa.gz, .fasta.gz, .fq.gz and .fastq.gz)''')
     parser.add_argument('-r', '--reference-required', action='store_true',
-        help='''ignore datasets without a reference genome''')
+        help='''ignore data sets without a reference genome''')
 
     args = parser.parse_args()
 
